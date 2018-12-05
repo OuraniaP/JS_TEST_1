017ccc0c3c674bf5b505283b3ad30215eeb2d958
OuraniaP <44147599+OuraniaP@users.noreply.github.com>
2018-12-05 16:05:51 +0200

    Add files via upload

---
+++ JS_TEST_1/script.js
@@ -0,0 +1,183 @@
+// //1a.
+// var age = 38;
+// //1b.
+// const name = "Ourania";
+// //1c.
+// var isMarried = false;
+// //1d.
+// var age = 38;
+// age++;
+// console.log(age++);
+// //1e. 
+// Yes, it is possible, if it is declared with "let" or "var".
+// //1f. 
+// No, it is not possible because it is declared with "const".
+
+// //2a.
+// Number.
+// //2b.
+// String.
+// //2c.
+// Boolean.
+// //2d.
+// String.
+// //2e. 
+// String.
+// //2f. 
+// Boolean.
+// //2g.
+// let x = "123";
+// x = Number(x);
+// //2h. 
+// x = String(x);
+
+// //3a. 
+// Variable not given a value. 
+// //3b. 
+// Not a Number.
+// //3c. 
+// Number. 
+// //3d. 
+// Undefined.
+// //3e.
+// Number.
+// //3f. 
+// Use the isNaN() function.
+// //3g. 
+// x === 4, checks value & type to be equal
+// //3h. 
+// x == null, checks the absence of a value
+
+// // 4a.
+// sum = 10 + 15 ;
+// // 4b.
+// 11
+// // 4c. 
+// ((3+4) * 2)
+// // 4d. 
+// 6 
+// // 4e. 
+// (20 - (8 - 2) - 4)
+// // 4f.
+// Hello,world 
+// // 4g.
+// 5
+// // 5a. 
+// Boolean.
+// // 5b.
+// False. 
+// // 5c. 
+// False.
+// // 5d. 
+// True. 
+// // 5e. 
+// False.
+// // 5f.
+// True.
+// // 5g.
+// Boolean.
+// // 5h.
+// False. 
+// // 5i.
+// True.
+// // 5j. 
+// True.
+// // 5k.
+// False.
+// // 5l.
+// a !== false
+// // 5m.
+// b !== false
+
+// // 6a.
+// arr1 = [];
+// // 6b.
+// 3
+// // 6c.
+// arr.splice(1, 1, 7);
+// // 6d.
+// arr.push(2);
+// // 6e.
+// arr.unshift(12);
+// // 6f.
+// console.log(arr[0]);
+// // 6g. 
+// for (i=0; i < arr.length; i++)
+// {
+//     console.log(arr[i]);
+// }
+// // 6h.
+// 0. 
+// // 6i.
+// Yes.
+// // 6j.
+// Yes. 
+// // 6k.
+// 3.
+// // 6l.
+// True.
+// // 6m. 
+// True.
+// // 6n.
+// 1, 1, 1
+
+// // 7a.
+// function four() {
+//     return 4;
+// }
+// // 7b.
+// Number.
+// // 7c.
+// Undefined.
+// // 7d.
+// 2.
+// // 7e.
+// 0. 
+// // 7f.
+// Undefined. 
+// // 7g.
+// String.
+// // 7h.
+// String.
+// // 7i.
+// Error.
+// // 7j.
+// Error.
+
+// // 8a.
+// String.
+// // 8b.
+// console.log(arr.length - 1);
+// // 8c.
+// function arr() 
+// {
+//     return arr.length * 2;
+// }
+// // 8d.
+// function name() {
+//     console.log("Hello," + name + "How are you doing?");
+// }
+// // 8e. 
+// Yes you can:
+// (`Hello,` ${name} `How are you doing?`);
+// // 8f. 
+// const ingredients = ["Banana", "Oats", "Milk"];
+// const commandString = "ls | cd | touch | cp";
+// console.log("For pancackes, you will need " + ingredients.join(" and "));
+// // 8g. 
+// console.log(commandString.split(" "));
+// // 8h.
+// function doubleArray (arr)
+// {
+// 	var newArr = [];
+// 	console.log(arr);
+// 	for (i=0; i < arr.length; i++)
+// 	{
+// 		newArr.push(arr[i]);
+// 	}	
+// 	for (i=0; i < arr.length; i++)
+// 	{
+// 		newArr.push(arr[i]);
+// 	}
+// 	return newArr;
+// }
